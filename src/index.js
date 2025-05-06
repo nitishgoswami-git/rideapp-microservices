4896ca1e27fc9ab92af2f037f895f93fff15f42b
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-05-06 18:40:28 +0000

    feat: add initial server setup with Express and dotenv configuration

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import {app} from './app.js';
+import {connectDB} from './db.js';
+
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
